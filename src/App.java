1a129eb94b0575789445a0a9e0e53f9eeb338187
ZouFitahiana <129894768+Fitahiana-herizo-RAKOTOMAMPIONONA@users.noreply.github.com>
2024-09-29 10:18:24 +0300

    resolve distance ok

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    static ArrayList<String> bibliotheque =new ArrayList<String>();
+
+    public static int resolveLenveisten(String str ,String str2){
+        int [][] matrice = new int[str.length()+1][str2.length()+1];
+        for (int i=0 ; i<= str.length(); i++){
+            for (int j=0 ; j<= str2.length(); j++){
+                if (i==0) {
+                    matrice[i][j]= j;
+                }else if(j==0){
+                    matrice[i][j] = i;
+                }else{
+                    matrice[i][j] = minimun_edits(matrice[i-1][j-1] + test_caractere(str.charAt(i-1) , str2.charAt(j-1) ) , matrice[i-1][j] + 1 , matrice[i][j-1]+1);
+                };
+            }
+        }
+        return matrice[str.length()][str2.length()];
+    }   
+
+    
+    public static int minimun_edits(int  ...nums){
+        return Arrays.stream(nums).min().orElse(Integer.MAX_VALUE);
+    }
+
+
+    public static int test_caractere(char c1 ,char c2){
+        return c1 == c2? 0 : 1;
+    }
+    public static void main(String[] args) {
+        String s1 = "herizo";
+        String s2 = "hepezo";
+        System.out.println(resolveLenveisten(s1, s2));
     }
 }
